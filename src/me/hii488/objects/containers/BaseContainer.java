2be8fdfad9070f82a3de28678282662a6690dcca
Laurence <hii488@btinternet.com>
2017-09-14 18:23:11 +0100

    Added missing methods

--- src/me/hii488/objects/containers/BaseContainer.java
+++ src/me/hii488/objects/containers/BaseContainer.java
@@ -3,8 +3,12 @@ package me.hii488.objects.containers;
 import java.awt.Graphics;
 import java.util.ArrayList;
 
+import me.hii488.controllers.GameController;
+import me.hii488.graphics.Camera;
 import me.hii488.interfaces.ITickable;
 import me.hii488.misc.Grid;
+import me.hii488.misc.Settings;
+import me.hii488.misc.Vector;
 import me.hii488.objects.entities.BaseEntity;
 
 public abstract class BaseContainer implements ITickable{
@@ -19,8 +23,32 @@ public abstract class BaseContainer implements ITickable{
 	
 	public BaseContainer(){}
 	
-	public abstract void onLoad();
-	public abstract void onUnload();
+	public void onLoad(){
+		if(grid.dimensions.getX() < GameController.windows[0].width && grid.dimensions.getY() < GameController.windows[0].height){
+			Camera.moveTo(new Vector(GameController.windows[0].width / 2 - grid.dimensions.getX() * (Settings.Texture.tileSize*Camera.scale/2),
+									 GameController.windows[0].height/ 2 - grid.dimensions.getY() * (Settings.Texture.tileSize*Camera.scale/2)));
+		}
+		endOfTick();
+		for (int i = 0; i < entities.size(); i++) {
+			entities.get(i).onLoad();
+		}
+	}
+	
+	public void onUnload(){}
+	
+	public void updateOnTick() {
+		for (int i = 0; i < entities.size(); i++) {
+			entities.get(i).updateOnTick();
+		}
+		grid.updateOnTick();
+	}
+	
+	public void updateOnSec() {
+		for (int i = 0; i < entities.size(); i++) {
+			entities.get(i).updateOnSec();
+		}
+		grid.updateOnSec();
+	}
 	
 	public void endOfTick(){
 		this.entities.removeAll(entitiesDestroyedInTick);
@@ -46,7 +74,7 @@ public abstract class BaseContainer implements ITickable{
 	public void removeEntity(BaseEntity e){
 		entitiesDestroyedInTick.add(e);
 	}
-
+	
 	public void render(Graphics g) {
 		grid.render(g);
 		for (int i = 0; i < entities.size(); i++) {
