cd81eca0e8789ce339f65c0bf0f0ff75704b45df
Laurence <hii488@btinternet.com>
2017-12-18 19:01:57 +0000

    Improved/added some functions
    Can't remember what

--- src/me/hii488/helpers/EntityHelper.java
+++ src/me/hii488/helpers/EntityHelper.java
@@ -4,11 +4,13 @@ import java.awt.Rectangle;
 import java.util.ArrayList;
 
 import me.hii488.gameWorld.World;
+import me.hii488.gameWorld.baseTypes.GeneralWorldContainer;
 import me.hii488.objects.entities.GeneralEntity;
 
 public class EntityHelper {
 	
 	@SuppressWarnings("unchecked")
+	@Deprecated
 	public static ArrayList<GeneralEntity> getCollidingEntities(GeneralEntity toCheck){
 		ArrayList<GeneralEntity> all = (ArrayList<GeneralEntity>) World.getCurrentWorldContainer().getEntities().clone();
 		
@@ -23,6 +25,37 @@ public class EntityHelper {
 		return collidingWith;
 	}
 	
+	public static ArrayList<GeneralEntity> getCollidingEntities(GeneralEntity toCheck, GeneralWorldContainer container){
+		@SuppressWarnings("unchecked")
+		ArrayList<GeneralEntity> all = (ArrayList<GeneralEntity>) container.getEntities().clone();
+		
+		ArrayList<GeneralEntity> collidingWith = new ArrayList<GeneralEntity>();
+		
+		for(GeneralEntity e : all){
+			if(e.collisionBox.intersects(toCheck.collisionBox)){
+				collidingWith.add(e);
+			}
+		}
+		
+		return collidingWith;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static ArrayList<GeneralEntity> getEntitiesIntersectingWithRect(Rectangle r, GeneralWorldContainer container){
+		ArrayList<GeneralEntity> all = (ArrayList<GeneralEntity>) container.getEntities().clone();
+		
+		ArrayList<GeneralEntity> collidingWith = new ArrayList<GeneralEntity>();
+		
+		for(GeneralEntity e : all){
+			if(e.collisionBox.intersects(r)){
+				collidingWith.add(e);
+			}
+		}
+		
+		return collidingWith;
+	}
+	
+	@Deprecated
 	@SuppressWarnings("unchecked")
 	public static ArrayList<GeneralEntity> getEntitiesIntersectingWithRect(Rectangle r){
 		ArrayList<GeneralEntity> all = (ArrayList<GeneralEntity>) World.getCurrentWorldContainer().getEntities().clone();
@@ -38,10 +71,23 @@ public class EntityHelper {
 		return collidingWith;
 	}
 
+	@Deprecated
 	public static boolean isOutOfContainer(GeneralEntity generalEntity) {
 		// Easiest way to check if it's still on the grid :
 		try{
 			World.getCurrentWorldContainer().grid.getTile(World.getCurrentWorldContainer().grid.getGridPositionOn(generalEntity.position)).getTileType();
+		}
+		catch(Exception e){
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public static boolean isOutOfContainer(GeneralEntity generalEntity, GeneralWorldContainer container) {
+		// Easiest way to check if it's still on the grid :
+		try{
+			container.grid.getTile(container.grid.getGridPositionOn(generalEntity.position)).getTileType();
 		}
 		catch(Exception e){
 			return true;
