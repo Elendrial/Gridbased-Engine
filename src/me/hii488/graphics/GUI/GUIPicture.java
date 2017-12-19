955eaa962524412bb84fc50a75d97d59e68c4424
Laurence <hii488@btinternet.com>
2017-12-19 01:04:10 +0000

    Increased functionality of GUIPicture

--- src/me/hii488/graphics/GUI/GUIPicture.java
+++ src/me/hii488/graphics/GUI/GUIPicture.java
@@ -8,7 +8,8 @@ import me.hii488.handlers.TextureHandler;
 public class GUIPicture extends GUIElement{
 
 	public String textureName = "";
-
+	public String textureKey;
+	
 	public String identifier, path = "textures/gui/";
 	public int states = 0;
 	public int currentState = 0;
@@ -19,19 +20,19 @@ public class GUIPicture extends GUIElement{
 	@Override
 	public void render(Graphics g) {
 		if(hidden) return;
-		g.drawImage(TextureHandler.getTexture(textureName + "_" + currentState), position.getX(), position.getY(), null);
+		g.drawImage(TextureHandler.getTexture(textureKey + "_" + currentState), position.getX(), position.getY(), null);
 	}
 	
 	public void setupTextures() {
 		String sanitizedName = textureName.split("\\.")[0];
+		if(textureKey == null) textureKey = sanitizedName;
+		
 		if(states > 0){
-			for(int i = 0; i < states; i++) TextureHandler.loadTexture(path, sanitizedName + "_" + i + "." + textureName.split("\\.")[1], this, sanitizedName + "_" + i);
+			for(int i = 0; i < states; i++) TextureHandler.loadTexture(path, sanitizedName + "_" + i + "." + textureName.split("\\.")[1], this, textureKey + "_" + i);
 		}
 		else{
-			TextureHandler.loadTexture(path, textureName, this, sanitizedName + "_0");
+			TextureHandler.loadTexture(path, textureName, this, textureKey + "_0");
 		}
-		
-		textureName = textureName.split("\\.")[0];
 	}
 
 	public String getTextureName() {
@@ -40,6 +41,15 @@ public class GUIPicture extends GUIElement{
 
 	public GUIPicture setTextureName(String textureName) {
 		this.textureName = textureName;
+		return this;
+	}
+	
+	public String getTextureKey() {
+		return textureKey;
+	}
+
+	public GUIPicture setTextureKey(String textureKey) {
+		this.textureKey = textureKey;
 		return this;
 	}
 
@@ -53,7 +63,7 @@ public class GUIPicture extends GUIElement{
 	}
 	
 	public GUIPicture setPath(String path) {
-		this.path = "textures/" + path;
+		this.path = path;
 		return this;
 	}
 	
